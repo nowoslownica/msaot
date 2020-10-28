e5035131cb400f03acca6df63b8fd6ee34e421ba
George Carpow <e.karpov@npo-echelon.ru>
2020-10-29 00:06:48 +0300

    some fix

---
+++ morphological/config.go
@@ -0,0 +1,8 @@
+package morphological
+
+type Config struct {
+	Cases  map[int]string
+	Tenses map[int]string
+	Persons map[int]string
+
+}
--- morphological/lemmas.go
+++ morphological/lemmas.go
@@ -1,8 +1,23 @@
 package morphological
 
+type NCase struct {
+	Case   string
+	Number string
+	Person string
+}
+
+type VCase struct {
+	Person string
+	Number string
+	Tense  string
+}
+
 type Lemma struct {
 	Normal string
-
+	Value  string
+	Post   string
+	nCase  *NCase
+	vCase  *VCase
 }
 
 func Build(word string) *Lemma {
