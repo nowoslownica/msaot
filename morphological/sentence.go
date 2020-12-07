c5153aceb9c80fd2e4611a84b0e5c442341a2be0
george <karpov@myservices.global>
2020-12-07 17:15:13 +0300

    syntax tree

--- morphological/sentence.go
+++ morphological/sentence.go
@@ -5,17 +5,51 @@ import (
 	"strings"
 )
 
-func BuildLemmaChains(tokens []*graphematical.Token) ([][]*Lemma, error) {
-	res := make([][]*Lemma, 0)
-	res = append(res, make([]*Lemma, 0))
+type SentenceLemmaChain struct {
+	ChainPosition int
+	Lemmas        []*Lemma
+}
+
+func BuildSentenceLemmaChains(chains [][]*LemmaOrStop) []SentenceLemmaChain {
+	res := make([]SentenceLemmaChain, 0)
+	for i, chain := range chains {
+		temp := make([]*Lemma, 0)
+		for _, lemma := range chain {
+			if lemma.Stop {
+				res = append(res, SentenceLemmaChain{
+					ChainPosition: i,
+					Lemmas:        temp,
+				})
+				temp = make([]*Lemma, 0)
+			}
+			temp = append(temp, lemma.Lemma)
+		}
+	}
+	return res
+}
+
+type LemmaOrStop struct {
+	Lemma *Lemma
+	Stop  bool
+}
+
+func BuildLemmaChains(tokens []*graphematical.Token) ([][]*LemmaOrStop, error) {
+	res := make([][]*LemmaOrStop, 0)
+	res = append(res, make([]*LemmaOrStop, 0))
 	for _, token := range tokens {
+		if token.Value == "." {
+			for i := range res {
+				res[i] = append(res[i], &LemmaOrStop{ Stop: true })
+			}
+			continue
+		}
 		lemmas, err := NormalizeAuto(strings.ToLower(token.Value))
 		if err != nil {
 			return nil, err
 		}
 		if len(lemmas) == 1 {
 			for i := range res {
-				res[i] = append(res[i], lemmas[0])
+				res[i] = append(res[i], &LemmaOrStop{ Lemma: lemmas[0], Stop: false })
 			}
 		}
 		// TODO: if more than 1 - create a doubled slice and copy previous values
--- morphological/sentence_test.go
+++ morphological/sentence_test.go
@@ -42,3 +42,43 @@ func TestBuildLemmaChains(t *testing.T) {
 		}
 	}
 }
+
+func TestBuildSentenceLemmaChains(t *testing.T) {
+	_, closeF, err := lexicon.InitMain()
+	if err != nil {
+		return
+	}
+	defer func() {
+		_ = closeF()
+	}()
+
+	empty := make([][]*Lemma, 0)
+	testCases := []struct{
+		Data []*graphematical.Token
+		Expected [][]*Lemma
+	}{
+		{[]*graphematical.Token{
+			{Value: "Я", Position: 0},
+			{Value: "есмь", Position: 1},
+			{Value: "пошёл", Position: 2},
+			{Value: "гулять", Position: 3},
+			{Value: "по", Position: 4},
+			{Value: "красивому", Position: 5},
+			{Value: "городу", Position: 6},
+			{Value: ".", Position: 7},
+		}, empty},
+	}
+	for _, test := range testCases {
+		lemmaArrs, err := BuildLemmaChains(test.Data)
+		if err != nil {
+			t.Error("Error while building lemma chains", err)
+		}
+		sentences := BuildSentenceLemmaChains(lemmaArrs)
+		for _, sentence := range sentences {
+			for _, lemma := range sentence.Lemmas {
+				fmt.Print(lemma.Value + " ")
+			}
+			fmt.Println(".")
+		}
+	}
+}
