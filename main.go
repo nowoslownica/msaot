ec1e44a004b6c8d92ca1311d40cd361cbe5ae6a8
george <karpov@myservices.global>
2020-12-07 22:36:54 +0300

    cli interface

--- main.go
+++ main.go
@@ -1,5 +1,82 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/eakarpov/msaot/graphematical"
+	"github.com/eakarpov/msaot/lexicon"
+	"github.com/eakarpov/msaot/morphological"
+	"github.com/eakarpov/msaot/syntax"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
 
+func main() {
+	app := &cli.App{
+		Name: "msaot",
+		Usage: "Interslavic automatic language tool",
+		Commands: []*cli.Command{
+			{
+				Name:    "lemma",
+				Aliases: []string{"l"},
+				Usage:   "Enter a word to get list of lemmas",
+				Action:  func(c *cli.Context) error {
+					word := c.Args().First()
+					if word != "" {
+						_, close, err := lexicon.InitMain()
+						if err != nil {
+							return err
+						}
+						defer close()
+						lemmas, err := morphological.NormalizeAuto(word)
+						if err != nil {
+							return err
+						}
+						for _, lemma := range lemmas {
+							fmt.Printf(`%s (%s)`, lemma.Normal, lemma.Pos)
+							fmt.Println()
+						}
+						return nil
+					}
+					return fmt.Errorf("no input")
+				},
+			},
+			{
+				Name:    "syntaxtree",
+				Aliases: []string{"st"},
+				Usage:   "Build a syntax tree of a sentence",
+				Action:  func(c *cli.Context) error {
+					sentence := c.Args().First()
+					if sentence != "" {
+						_, close, err := lexicon.InitMain()
+						if err != nil {
+							return err
+						}
+						defer close()
+						tokenizer := graphematical.Tokenizer{}
+						tokenizer.Parse(sentence)
+						LoS, err := morphological.BuildLemmaChains(tokenizer.Items)
+						if err != nil {
+							return err
+						}
+						sentences := morphological.BuildSentenceLemmaChains(LoS)
+						for _, sntnce := range sentences {
+							tree, err := syntax.BuildSyntaxTree(sntnce.Lemmas)
+							if err != nil {
+								return err
+							}
+							fmt.Println(tree)
+						}
+						return nil
+					}
+					return fmt.Errorf("no input")
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
